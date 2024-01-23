c5c946149068d0e4adfa2c084867355d245b5123
ornekeda02 <154329335+ornekeda02@users.noreply.github.com>
2024-01-23 04:11:21 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+function Buton4(){
+    let A=document.getElementById('TSE-2');
+    let B='TSE-7.jpg';
+    A.setAttribute('src',B)
+}
+
+function Buton3(){
+    let A=document.getElementById('TSE-3');
+    let B='TSE-9.jpg';
+    A.setAttribute('src',B)
+}
+
+
+function Buton5(){
+    let A=document.getElementById('TSE-4');
+    let B='TSE-8.jpg';
+    A.setAttribute('src',B)
+}
+
+function Buton(){
+    let A=document.getElementById('Yapı Bilgisi');
+    let B='Yapı Bilgisi-5.jpg';
+    A.setAttribute('src',B)
+}
+
+function Buton1(){
+    let A=document.getElementById('Yapı Bilgisi-1');
+    let B='Yapı Bilgisi-6.jpg';
+    A.setAttribute('src',B)
+}
+
+function Buton2(){
+    let A=document.getElementById('Yapı Bilgisi-2');
+    let B='Yapı Bilgisi-7.jpg';
+    A.setAttribute('src',B)
+}
+
+function openNav() {
+    document.getElementById("B").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    }
+    
+    function closeNav() {
+    document.getElementById("B").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    }
+
+
+    
